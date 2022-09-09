d76983e16c55dad342b670259942398be98a6732
bangokun <108464869+bangokun@users.noreply.github.com>
2022-09-09 18:50:36 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,9 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
-╰────
-`.trim()) // Tambah sendiri kalo mau
+╭═┅═「 Donasi • Pulsa/Dana 」
+┝IM3   [085738639969]
+┝Dana  [085738639969]
+╰═┅═━═┅═━═┅═━––––––
+`.trim())
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
