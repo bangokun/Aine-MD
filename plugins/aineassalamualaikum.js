39b50da0d3d35d5d7be6d322ee030f9a42a0ac3d
bangokun <108464869+bangokun@users.noreply.github.com>
2022-09-11 21:05:52 +0800

    Update aineassalamualaikum.js

--- plugins/aineassalamualaikum.js
+++ plugins/aineassalamualaikum.js
@@ -1,10 +1,10 @@
 /*let fs = require('fs')
 let handler = async (m) => {
-let assalamualaikum = fs.readFileSync('./mp3/WhatsApp-Audio-2021-03-02-at-20.46.15.opus')
+let assalamualaikum = fs.readFileSync('./mp3/punten.opus')
 conn.sendFile(m.chat, assalamualaikum, '', '', m, true)
 }
 
-handler.customPrefix = /^(assalamualaikum|assallamualaikum|assalamu\'alaikum|as\'salamualaikum)$/i
+handler.customPrefix = /^(makasih|makasi)$/i
 handler.command = new RegExp
 handler.limit = true
 
