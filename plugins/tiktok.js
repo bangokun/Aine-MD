60c8cd8679a8e091e4c46395fecfa1715eb6e58f
bangokun <108464869+bangokun@users.noreply.github.com>
2022-09-16 22:30:56 +0800

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,17 +1,16 @@
-let handler = async(m,{text, conn}) => {
-if (!text) throw 'masukkan link nya'
-let p = await require('caliph-api').downloader.tiktok(text)
-let nih = ` *TIKTOK DOWNLOADER BY CHONIX*
-
- *Title:* ${p.title}
- *Author:* ${p.author}
-`
-    conn.sendMessage(m.chat, { video: { url: p.nowm }, caption: nih }, { quoted: m })   
+const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
+    tiktokdlv3(args[0]).then(r => {
+    let video = r.video.no_watermark
+    conn.sendFile(m.chat, video, '', `*${wm}*`, m)
+    })
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|ttk)$/i
-
 handler.limit = true
+handler.group = false
+
+handler.command = /^(tt|tiktok|tik)$/i
 
 module.exports = handler
